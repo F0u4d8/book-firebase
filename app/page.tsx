d7b23963931582dd975225ed3d17c9c89b0e615a
F0u4d8 <f0u4d08@gmail.com>
2024-12-14 16:44:31 +0100

    fixed login

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,20 @@
 import AppLogo from "@/components/shared/app-logo";
-import { lusitana } from "@/components/shared/fonts";
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
-import Link from "next/link";
+import LoginForm from "@/components/shared/login-form";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col ">
-      <div className="flex h-20 shrink-0 items-center rounded-lg p-4 md:h-40 bg-secondary">
-        <AppLogo />
-      </div>
-      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg  px-6 py-10 md:w-2/5 md:px-20">
-          <p
-            className={`${lusitana.className} text-xl md:text-3xl md:leading-normal`}
-          >
-            <strong>Welcome to Next 15 Admin Dashboard.</strong>
-          </p>
-          <Button asChild className="w-1/2">
-            <Link href="/login">
-              <span>Log in</span>
-            {/*<ArrowRightIcon className="w-6" />*/ }  
-            </Link>
-          </Button>
-        </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          <Image
-            src="/BOUK-05.png"
-            width={1000}
-            height={760}
-            alt="Screenshots of the dashboard project showing desktop version"
-            className="hidden md:block"
-          />
-       
-        </div>
-      </div>
+    <div className="flex justify-center items-center min-h-screen w-full ">
+    <main className="w-full max-w-md mx-auto">
+      <Card>
+        <CardHeader className="space-y-4 flex justify-center items-center">
+          <AppLogo />
+        </CardHeader>
+        <CardContent className="space-y-4">
+          <LoginForm />
+        </CardContent>
+      </Card>
     </main>
+  </div>
   );
 }
