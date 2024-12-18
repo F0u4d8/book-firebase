4a842e1d2e100edd0f427288cf0930e4fd54036a
F0u4d8 <f0u4d08@gmail.com>
2024-12-18 15:49:20 +0100

    eror handling

--- components/shared/stores/storeLink.tsx
+++ components/shared/stores/storeLink.tsx
@@ -8,7 +8,7 @@ const StoreLink = ({ id, name , type }: { id: string , image?: string ,name?: st
 
     return (
         <td
-            className="whitespace-nowrap py-5 pl-4 pr-3 text-sm group-first-of-type:rounded-md group-last-of-type:rounded-md sm:pl-6 cursor-pointer"
+            className="whitespace-nowrap py-5  text-sm group-first-of-type:rounded-md group-last-of-type:rounded-md sm:pl-6 cursor-pointer"
             onClick={() => router.push(`/${type}/${id}`)}
         >
           <div className="flex items-center gap-3">
