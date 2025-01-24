72921335379e38e641ad36912c6e34ddbefbef4e
F0u4d8 <f0u4d08@gmail.com>
2025-01-24 19:40:45 +0100

    uploaded changes 2

--- firebaseClient.ts
+++ firebaseClient.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAcLj4WQrgjAW2cGhwBg705_ErQUD0QUUY",
-    authDomain: "book-app-f302f.firebaseapp.com",
-    projectId: "book-app-f302f",
-    storageBucket: "book-app-f302f.appspot.com",
-    messagingSenderId: "296802698514",
-    appId: "1:296802698514:web:148bcf847bfe116d49dfcb",
-    measurementId: "G-76PX6D7FRT"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENTID
   };
 
 const app = initializeApp(firebaseConfig);
